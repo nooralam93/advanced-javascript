0f9f7e7a58aa4c08e0d1a5c9b1b910c3670eef93
nooralam93 <nooralomtex@gmail.com>
2020-10-06 08:36:02 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,27 @@
+
+// falsy:
+// 0
+// ""
+// null
+// undefined
+// NaN
+// false
+// truty:
+// " "
+// "0"
+// []
+
+// const age = 0;
+// const name = NaN;
+let nums = []
+// console.log(age);
+// console.log(name)
+let name = 0;
+console.log(nums)
+
+if(nums){
+    console.log('condition is true')
+}
+else{
+    console.log('condition is false')
+}
